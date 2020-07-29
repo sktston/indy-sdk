aaf30366690195c8c09a993c5cbe8af6c6d250ad
khujin <khujin@gmail.com>
2020-07-29 11:49:49 +0900

    First commit

--- vcx/wrappers/java/src/main/java/com/evernym/sdk/vcx/vcx/VcxApi.java
+++ vcx/wrappers/java/src/main/java/com/evernym/sdk/vcx/vcx/VcxApi.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
-
+//
 public class VcxApi extends VcxJava.API {
     private static final Logger logger = LoggerFactory.getLogger("VcxApi");
     private VcxApi() {
